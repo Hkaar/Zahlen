3294b0e8b6f8585d1ff3913ed93c8e897ca09d3a
Hkaar <HkaarDev@gmail.com>
2024-05-17 16:51:51 +0800

    Added type definitons to stat module in zahlen

--- src/zahlen/stat.py
+++ src/zahlen/stat.py
@@ -1,40 +1,25 @@
 import math
 import time
 
-from typing import Any, Collection, Tuple, Union
-from dataclasses import dataclass, field
+from .defs import Result, SumOfSquares
+from typing import Any, Collection
 
-@dataclass(frozen=True)
-class SumOfSquaresResult:
-    """A class to represent the result of the 'ss' function"""
-
-    sum_x: Union[int, float] = 0
-    sum_y: Union[int, float] = 0
-    n: int = 0
-    xy: Tuple[Union[int, float], ...] = field(default_factory=tuple)
-    x2: Tuple[Union[int, float], ...] = field(default_factory=tuple)
-    y2: Tuple[Union[int, float], ...] = field(default_factory=tuple)
-    sum_xy: Union[int, float] = 0
-    sum_x2: Union[int, float] = 0
-    sum_y2: Union[int, float] = 0
-    ss_xy: Union[int, float] = 0
-    ss_x2: Union[int, float] = 0
-    ss_y2: Union[int, float] = 0
-
-    def __iter__(self):
-        for val in self.__dataclass_fields__.keys():
-            yield getattr(self, val)
-
-def ss(x: Collection[int|float], y: Collection[int|float]) -> SumOfSquaresResult:
+def ss(x: Collection[int|float], y: Collection[int|float]) -> Result[SumOfSquares]:
     if not isinstance(x, Collection) or not isinstance(y, Collection):
-        raise TypeError("x, y mus be a iterable!")
+        return Result(err=TypeError("x, y mus be a iterable!"))
+    
+    if len(x) <= 1 and len(y) <= 1:
+        return Result(err=ValueError("X and Y must be atleast 2 elements long!"))
     
     if len(x) != len(y):
-        raise ValueError("X and Y must be of the same length!")
+        return Result(err=ValueError("X and Y must be of the same length!")) 
     
     sum_x = sum(x)
     sum_y = sum(y)
     n = len(x)
+
+    average_x = sum_x/n
+    average_y = sum_y/n
 
     xy = tuple(xi*yi for xi, yi in zip(x, y))
     x2 = tuple(xi**2 for xi in x)
@@ -48,24 +33,35 @@ def ss(x: Collection[int|float], y: Collection[int|float]) -> SumOfSquaresResult
     ss_x2 = sum_x2 - ((sum_x**2)/n)
     ss_y2 = sum_y2 - ((sum_y**2)/n)
 
-    return SumOfSquaresResult(
-        n = n, sum_x = sum_x, sum_y = sum_y,
-        xy = xy, x2 = x2, y2 = y2,
-        sum_xy = sum_xy, sum_x2 = sum_x2, sum_y2 = sum_y2,
-        ss_xy = ss_xy, ss_x2 = ss_x2, ss_y2 = ss_y2
-    )
+    return Result(SumOfSquares(
+        sum_x, sum_y, n, average_x, average_y, xy, x2, y2, sum_xy, sum_x2, sum_y2, ss_xy, ss_x2, ss_y2
+    ))
 
-def corelation(x: Collection[int|float], y: Collection[int|float]) -> int|float:
-    xy, x2, y2 = ss(x, y)
+def corelation(x: Collection[int|float], y: Collection[int|float]) -> Result[int|float]:
+    base = ss(x, y)
+
+    if not base.ok:
+        return Result(err=base.err)
+    
+    xy = base.ok.ss_xy
+    x2 = base.ok.ss_x2
+    y2 = base.ok.ss_y2
 
     result = xy/math.sqrt((x2*y2))
-    return result
+    return Result(result)
 
-def determinate(x: Collection[int|float], y: Collection[int|float]) -> int|float:
-    xy, x2, y2 = ss(x, y)
+def determinate(x: Collection[int|float], y: Collection[int|float]) -> Result[int|float]:
+    base = ss(x, y)
+
+    if not base.ok:
+        return Result(err=base.err)
+    
+    xy = base.ok.ss_xy
+    x2 = base.ok.ss_x2
+    y2 = base.ok.ss_y2
 
     result = (xy**2)/(x2*y2)
-    return result
+    return Result(result)
 
 if __name__ == "__main__":
     x = [5, 10, 11, 7]
@@ -73,3 +69,9 @@ if __name__ == "__main__":
 
     start = time.time()
     n = ss(x, y)
+
+    print(corelation(x, y))
+
+    print(determinate(x, y))
+
+    print(time.time() - start)
