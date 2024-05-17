c69fce1a4b15f6c495d4f8546a5c2d8dd7019581
Hkaar <HkaarDev@gmail.com>
2024-05-17 16:46:29 +0800

    Updated __main__.py in src

--- src/__main__.py
+++ src/__main__.py
@@ -6,5 +6,7 @@ from pathlib import Path
 if __name__ == "__main__":
     paths = [Path(sys.path[0], "console")]
 
-    Console.auto_import(paths, "console")
-    Console.exec()
+    kernel = Console(description="Zahlen console commands")
+
+    kernel.auto_import(paths, "console")
+    kernel.exec_()
